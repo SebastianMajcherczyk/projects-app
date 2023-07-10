7f762404c09ebd39f46f5288cae8422067b8c04a
SebastianMajcherczyk <seb.majcherczyk@gmail.com>
2023-07-10 12:32:19 +0200

    login-panel: error alerts

--- src/components/login-panel/LoginPanel.jsx
+++ src/components/login-panel/LoginPanel.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
 import './LoginPanel.css';
+import * as yup from 'yup';
 
 export const LoginPanel = () => {
-	const onSubmit = values => {};
-	const { handleSubmit, handleChange, values } = useFormik({
+    const onSubmit = async (values, actions) => {
+		console.log(values);
+		console.log(actions);
+		await new Promise((r) => setTimeout(r, 5000));
+		actions.resetForm()
+	};
+    
+    const validationSchema = yup.object({
+        email: yup.string().email('Email nieprawidłowy').required('Uzupełnij email'),
+        password: yup.string().required('Podaj prawidłowe hasło'),
+    })
+
+	const { handleSubmit, handleChange, handleBlur, values, errors, touched } = useFormik({
 		initialValues: {
 			email: '',
 			password: '',
 		},
+		validationSchema,
 		onSubmit,
 	});
 	return (
 		<div>
-			<form className='login-form' onSubmit={handleSubmit}>
+			<form className='login-form' onSubmit={handleSubmit} noValidate>
 				<div>
 					<label htmlFor='email'>Email</label>
 					<input
@@ -22,7 +35,9 @@ export const LoginPanel = () => {
 						id='email'
 						value={values.email}
 						onChange={handleChange}
+                        onBlur={handleBlur}
 					/>
+                    <p className={errors.email && touched.email ? 'error' : "hidden"}>{errors.email && touched.email ? errors.email : "Placeholder"}</p>
 				</div>
 				<div>
 					<label htmlFor='password'>Password</label>
@@ -32,8 +47,10 @@ export const LoginPanel = () => {
 						id='password'
 						value={values.password}
 						onChange={handleChange}
+                        onBlur={handleBlur}
 					/>
 				</div>
+                <p className={errors.password && touched.password ? 'error' : 'hidden'}>{errors.password && touched.password ? errors.password : "Placeholder"}</p>
 				<button type='submit' className='button-55'>Login</button>
 			</form>
 		</div>
