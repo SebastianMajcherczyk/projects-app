6fc4bfdd19d7e5c936c9fde189811b752a499154
SebastianMajcherczyk <seb.majcherczyk@gmail.com>
2023-07-17 17:43:59 +0200

    PA-3: Close pop-up when clicked outside

--- src/components/pop-up-form/PopUpForm.jsx
+++ src/components/pop-up-form/PopUpForm.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import './PopUpForm.css';
 
-export const PopUpForm = () => {
+export const PopUpForm = ({setIsOpenPopup}) => {
+
 	const { handleChange, handleBlur, touched, values, errors } = useFormik({
 		initialValues: {
 			title: '',
@@ -14,34 +15,49 @@ export const PopUpForm = () => {
 			team: Yup.number().min(1, 'Minimum 1 znak').required('Pole wymagane'),
 		}),
 	});
+    const popupRef = useRef(null);
+    const handleClickOutside = (event) => {
+        if (popupRef.current && !popupRef.current.contains(event.target)) {
+            setIsOpenPopup(false);
+        }
+      };
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      });
 	return (
 		<>
-			<form className='popup-form'>
-				<div>
-					<label htmlFor='title'>Nazwa projektu</label>
-					<input
-						id='title'
-						value={values.title}
-						onChange={handleChange}
-						onBlur={handleBlur}
-					/>
-					<p className={errors.title && touched.title ? 'error' : 'hidden'}>
-						{errors.title && touched.title ? errors.title : 'Placeholder'}
-					</p>
-				</div>
-				<div>
-					<label htmlFor='team'>Wielkość zespołu</label>
-					<input
-						id='team'
-						value={values.team}
-						onChange={handleChange}
-						onBlur={handleBlur}
-					/>
-					<p className={errors.team && touched.team ? 'error' : 'hidden'}>
-						{errors.team && touched.team ? errors.team : 'Placeholder'}
-					</p>
-				</div>
-			</form>
+			<div className='popup-container' ref={popupRef}>
+				<form className='popup-form'>
+					<div>
+						<label htmlFor='title'>Nazwa projektu</label>
+						<input
+							id='title'
+							value={values.title}
+							onChange={handleChange}
+							onBlur={handleBlur}
+						/>
+						<p className={errors.title && touched.title ? 'error' : 'hidden'}>
+							{errors.title && touched.title ? errors.title : 'Placeholder'}
+						</p>
+					</div>
+					<div>
+						<label htmlFor='team'>Wielkość zespołu</label>
+						<input
+							id='team'
+							value={values.team}
+							onChange={handleChange}
+							onBlur={handleBlur}
+						/>
+						<p className={errors.team && touched.team ? 'error' : 'hidden'}>
+							{errors.team && touched.team ? errors.team : 'Placeholder'}
+						</p>
+					</div>
+					<button type='submit'>Dodaj</button>
+				</form>
+			</div>
 		</>
 	);
 };
